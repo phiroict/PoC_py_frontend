b22251278b097d37f8ec75b9ddc962fe60e03eb2
Philip Rodrigues <phiroict@gmail.com>
2021-09-23 12:44:56 +1200

    Added env variable.

--- listener.py
+++ listener.py
@@ -5,10 +5,11 @@ import requests
 app = Flask(__name__)
 backend_service = os.getenv('BACKEND_SERVICE', 'localhost')
 backend_port = os.getenv('BACKEND_PORT', '4000')
+environment_name = os.getenv('ENV_SET', 'DEV')
 
 @app.route("/")
 def hello_world():
-    response = requests.get("http://{}:{}".format(backend_service, backend_port))
+    response = requests.get("http://{}:{} - ENV: {}".format(backend_service, backend_port, environment_name))
     return response.text
 
 
